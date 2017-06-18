12b05bdad72015c93ae4d11003fe1d766e88bf36
myomi <myomit@gmail.com>
2017-06-18 14:45:53 +0900

    Promisfied build script.

--- scripts/build.example.js
+++ scripts/build.example.js
@@ -3,45 +3,71 @@ const path = require("path");
 const rimraf = require("rimraf");
 const sass = require("node-sass");
 const packageImporter = require('node-sass-package-importer');
+const util = require("util");
 
-function renderSass(basedir) {
-    fs.readdir(basedir, (err, files) => {
-        if (err) throw err;
+const readdir = util.promisify(fs.readdir);
+const render = util.promisify(sass.render);
+const stat = util.promisify(fs.stat);
+const rimrafP = util.promisify(rimraf);
+const writeFile = util.promisify(fs.writeFile);
 
-        files.forEach((f) => {
-            const filePath = path.join(basedir, f);
-            const stat = fs.statSync(filePath);
-            if (stat.isDirectory()) renderSass(filePath);
+/*
+ * main
+ */
+findScss("example")
+.then((files) => {
+    files.forEach(async (f) => {
+        const outFile = f.replace(/\.scss$/, ".css");
+        // clean
+        await rimrafP(outFile);
+        // sass
+        const result = await render({
+            file: f,
+            outputStyle: "expanded",
+            outFile: outFile,
+            indentWidth: 4,
+            sourceMapEmbed: true,
+            importer: packageImporter()
+        });
+        // write
+        await writeFile(outFile, result.css);
+    });
+})
+.catch((err) => {
+    console.error(err);
+    process.exit(-1);
+});
 
-            if (stat.isFile() && filePath.match(/\.scss$/)) {
-                const outFile = filePath.replace(/\.scss$/, ".css");
+/*
+ * find .scss files from basedir
+ * @param basedir path of the target directory.
+ */
+async function findScss(basedir) {
+    const files = await readdir(basedir);
 
-                // clean
-                rimraf(outFile, (err) => {
-                    if (err) throw err;
-
-                    // sass
-                    sass.render({
-                        file: filePath,
-                        outputStyle: "expanded",
-                        outFile: outFile,
-                        indentWidth: 4,
-                        sourceMapEmbed: true,
-                        importer: packageImporter()
-                    }, (err, result) => {
-                        if (err) throw err;
-                        fs.writeFile(outFile, result.css);
-                    });
-                });
-            }
+    return Promise.all(files.map(async (f) => {
+        const filePath = path.join(basedir, f);
+        const status = await stat(filePath);
+        if (status.isDirectory()) {
+            return await findScss(filePath);
+        } else if (status.isFile() && filePath.match(/\.scss$/)) {
+            return filePath;
+        }
+    })).then((result) => {
+        // flatten and filtering only scss files.
+        return flatten(result).filter((e) => {
+            return e;
         });
     });
 }
 
-const targets = [
-    "example"
-];
-
-targets.forEach((t) => {
-    renderSass(t);
-});
+/*
+ * flatten nested array
+ * @param array src
+ * @return array flattened
+ */
+function flatten(array) {
+    return array.reduce(function (p, c) {
+        return Array.isArray(c) ? p.concat(flatten(c)) : p.concat(c);
+    }, []);
+}
